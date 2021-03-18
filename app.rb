378d39f6e45a93aeadc8fabb717e26aed9bf0ed0
savanna chiang <savannaac@gmail.com>
2021-03-17 20:56:41 -0500

    complete

--- app.rb
+++ app.rb
@@ -2,4 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get ("/") { erb :index }
+
+    get ("/new") { erb :create_puppy }
+
+    post "/puppy" do
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        erb :display_puppy
+    end
 end
